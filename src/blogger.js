285af3910f7249d1ab5c9837335c76de42cd14c7
Jan-Erik Pedersen <jannik@mobile-dna.com>
2020-12-12 21:59:34 +0100

    Fix syntax error

--- src/blogger.js
+++ src/blogger.js
@@ -12,25 +12,25 @@
         function get() {
           var form = $('#buy-art-form input');
           var message = '';
-          var divs = $(#buy - art - form form - group ');
-            for (var i = 0, n = divs.size(); i < n; i++) {
-              var div = lines[i];
-              message += div.getElementsByTagName('label')[0].value;
-              message += ': ' + div.getElementsByTagName('input')[0].value;
-              message += '\n';
-            }
-
-            $.post('https://www.blogger.com/contact-form.do', {
-                name: form[1].value,
-                email: form[2].value,
-                item: item,
-                message: message,
-                blogID: '5799764146171352736'
-              },
-              function(data, status) {
-                alert("Data: " + data + "\nStatus: " + status);
-              }
-            );
+          var divs = $('#buy-art-form form-group');
+          for (var i = 0, n = divs.size(); i < n; i++) {
+            var div = lines[i];
+            message += div.getElementsByTagName('label')[0].value;
+            message += ': ' + div.getElementsByTagName('input')[0].value;
+            message += '\n';
           }
-          return get;
+
+          $.post('https://www.blogger.com/contact-form.do', {
+              name: form[1].value,
+              email: form[2].value,
+              item: item,
+              message: message,
+              blogID: '5799764146171352736'
+            },
+            function(data, status) {
+              alert("Data: " + data + "\nStatus: " + status);
+            }
+          );
         }
+        return get;
+      }
