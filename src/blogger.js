fd16e0dcd8fa143076b1e95192ae640143cc78f6
Jan-Erik Pedersen <jannik@mobile-dna.com>
2020-12-19 11:29:16 +0100

    bug fix

--- src/blogger.js
+++ src/blogger.js
@@ -37,7 +37,7 @@ function buy_art_popup(item) {
 function show_or_hide_address(event) {
   var input = event.target;
   var hidden = input.value != 'chronopost';
-  var groups = $('#buy-art-form>div.form-group.address')[0];
+  var groups = $('#buy-art-form>div.form-group.address');
   for (var i = 0, n = groups.size(); i < n; i++) {
     var group = groups[i]
     group.hidden = hidden;
